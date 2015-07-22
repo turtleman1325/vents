79897cbe92f47ce9509b1cd45be63d2ea44eb541
Art <art19ad4@gmail.com>
2015-07-22 14:19:05 -0700

    Updated functioning save for user_id in posts column

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20150722182432) do
     t.integer  "user_id"
   end
 
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
