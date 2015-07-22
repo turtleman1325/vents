834a656589d295d7e68b58e3cf2ae38edefe04d4
Art <art19ad4@gmail.com>
2015-07-22 11:57:49 -0700

    Added devise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,6 +25,7 @@ class PostsController < ApplicationController
   # POST /posts.json
   def create
     @post = Post.new(post_params)
+    @post.save
 
     respond_to do |format|
       if @post.save
@@ -69,6 +70,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :body)
+      params.require(:post).permit(:title, :body, :user_id)
     end
 end
