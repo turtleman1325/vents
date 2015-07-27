6ab5169723cfa00e20dd6360c84ec2c3db3eb256
turtleman1325 <art19ad4@gmail.com>
2015-07-27 16:50:09 -0700

    Revert "Add devise"

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,7 +25,6 @@ class PostsController < ApplicationController
   # POST /posts.json
   def create
     @post = Post.new(post_params)
-    @post.user_id = current_user.id
 
     respond_to do |format|
       if @post.save
@@ -70,6 +69,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :body, :user_id)
+      params.require(:post).permit(:title, :body)
     end
 end
